bf6b846e5bc8832e59529445844d9079344ff689
Khair Ahammed <khair@brainstation-23.com>
2024-05-09 20:20:39 +0600

    Add docker files and refactored main.py

--- main.py
+++ main.py
@@ -11,16 +11,11 @@ class Input(BaseModel):
     tool_wear_mins: int
     product_type: str 
 
-
-# Maintenance prediction model and preprocessing loading
-model = pickle.load(open('maintenance-failure-model.pkl', 'rb'))
-encoder = pickle.load(open('encoder.pkl', 'rb'))
-
-app =FastAPI()
-
-
-@app.post('/predict_failure')
-def predict_failure(data: Input):
+def modelPipeline(data):
+    # Maintenance prediction model and preprocessing loading
+    model = pickle.load(open('maintenance-failure-model.pkl', 'rb'))
+    encoder = pickle.load(open('encoder.pkl', 'rb'))
+    
     input_list = [[
         data.air_temperature_k,
         data.process_temperature_k,
@@ -29,11 +24,8 @@ def predict_failure(data: Input):
         data.tool_wear_mins,
         encoder.transform([[data.product_type]])[0][0],
     ]]
-    print(input_list)
-
+   
     prediction = model.predict(input_list)
-
-    print(prediction)
     mapping = {
         0: 'Heat Dissipation Failure',
         1: 'No Failure',
@@ -41,10 +33,19 @@ def predict_failure(data: Input):
         3: 'Power Failure',
         4: 'Random Failures',
         5: 'Tool Wear Failure' 
-
     }
+    return mapping[prediction.item()]
 
-    return {"prediction": mapping[prediction.item()]}
+app =FastAPI()
+
+
+@app.post('/predict_failure')
+def predict_failure(data: Input):
+    
+    
+    prediction = modelPipeline(data)
+
+    return {"prediction": prediction}
 
 
 
